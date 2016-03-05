c0d7d8a41adce2454fcd80afe00529f1d674f6f3
Zachary Yedidia <zyedidia@gmail.com>
2016-03-05 16:29:04 -0500

    Use curl to install termbox instead of git

--- build-termbox.sh
+++ build-termbox.sh
@@ -1,11 +1,15 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 if [ ! -f libtermbox.a ] || [ "$1" == "-f" ]; then
-    git clone https://github.com/nsf/termbox.git termbox_c_src
-    cd termbox_c_src
+    echo "Building Termbox"
+    rm -rf termbox-master
+    curl -L --progress-bar https://github.com/nsf/termbox/archive/master.tar.gz > termbox.tar.gz
+    tar -xf termbox.tar.gz
+    rm termbox.tar.gz
+    cd termbox-master
     ./waf configure
     ./waf install --targets=termbox_static
     mv build/src/libtermbox.a ..
     cd ..
-    rm -rf termbox_c_src
+    rm -rf termbox-master
 fi
