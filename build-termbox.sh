f139f61f078c647bbd7c08619b999e8d305f5171
Nafees10 <nafeeshassan16@gmail.com>
2019-02-28 00:22:37 +0500

    Fixed a bug: when building a package dependent on termbox-d, it would build the libtermbox.a library in the dependent package's dir, instead of its own dir, causing an error because it searches for libtermbox.a in its own dir
    Signed-off-by: Nafees10 <nafeeshassan16@gmail.com>

--- build-termbox.sh
+++ build-termbox.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
+DIR=$(dirname "$0")
+cd "$DIR"
 if [ ! -f libtermbox.a ] || [ "$1" == "-f" ]; then
     echo "Building Termbox"
     rm -rf termbox-master
